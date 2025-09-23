ef63bf6cd475b997b8c41ac10305203035cb4f04
KOCAOGLU Manolia <157006573+manolia-kocaoglu@users.noreply.github.com>
2025-09-23 14:50:14 +0200

    Refactor saveSVG API endpoint and update collection rules; implement SVG saving functionality in generate.astro

--- src/pages/api/saveSVG.js
+++ src/pages/api/saveSVG.js
@@ -6,7 +6,7 @@ export async function POST({ request }) {
   console.log("Received data to save:", data);
   try {
     const record = await pb
-      .collection(Collections.Svg)
+      .collection(Collections.Save)
       .create(data);
     console.log("SVG saved with ID:", record.id);
 
